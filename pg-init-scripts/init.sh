803910ac271ab289931dae0e7f273218a7f1a8de
Abdessamad Laamimi <ablaamim@c3r8p5.1337.ma>
2023-09-15 22:29:31 +0100

    infra done

--- pg-init-scripts/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Create the directory and set ownership to pgadmin
-mkdir -p /var/lib/pgadmin/azurecredentialcache
-chown pgadmin:pgadmin /var/lib/pgadmin/azurecredentialcache
--- pgadmin/config/config_local.py
+++
@@ -1,4 +0,0 @@
-# pgadmin/config/config_local.py
-
-# Enable server mode
-SERVER_MODE = True
